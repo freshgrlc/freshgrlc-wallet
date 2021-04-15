b00a6d138b9762572049613c555ae0bcd53184dd
Erik van `t Wout <erik@ewnetworks.eu>
2021-04-15 02:34:24 +0200

    * More opcodes

--- opcodes.py
+++ opcodes.py
@@ -1,13 +1,24 @@
 
-OP_0            = 0x00
-OP_FALSE        = OP_0
+OP_0                    = 0x00
+OP_FALSE                = OP_0
 
-OP_PUSHDATA1    = 0x4c
-OP_PUSHDATA     = OP_PUSHDATA1
+OP_PUSHDATA1            = 0x4c
+OP_PUSHDATA             = OP_PUSHDATA1
 
-OP_DUP          = 0x76
-OP_EQUAL        = 0x87
-OP_EQUALVERIFY  = 0x88
-OP_HASH160      = 0xa9
-OP_CHECKSIG     = 0xac
+OP_IF                   = 0x63
+OP_ELSE                 = 0x67
+OP_ENDIF                = 0x68
 
+OP_DROP                 = 0x75
+OP_DUP                  = 0x76
+
+OP_EQUAL                = 0x87
+OP_EQUALVERIFY          = 0x88
+
+OP_NOT                  = 0x91
+
+OP_HASH160              = 0xa9
+OP_CHECKSIG             = 0xac
+OP_CHECKSIGVERIFY       = 0xad
+
+OP_CHECKLOCKTIMEVERIFY  = 0xb1
